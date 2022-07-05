b59b2fe0f253a64c35074935c1d7f59df3544b28
DorahRotich <rotichkurgat@gmail.com>
2022-07-05 13:06:37 -0700

    adds

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *add - entry point
+ * *@i: print int i
+ * *@k: print int k
+ * *Return: always 0
+ * 
+ **/
+int add(int i, int k)
+{
+	return (i + k);
+}
