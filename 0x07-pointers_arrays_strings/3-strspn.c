69fbd80feb8f66bebb2d9b284dfb0d26223ada2f
DorahRotich <rotichkurgat@gmail.com>
2022-07-18 19:04:16 +0000

    length of substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,19 +1,44 @@
 #include "main.h"
 
 /**
- * _strspn - function that gets the length of a substring
- * @s: initial segment of the segment
- * @accept: the bytes to accept
- * Return: length
- */
+ * _strspn - Locates a character in a string
+ * @s: This is the main C string to be scanned.
+ * @accept: This is the string containing the list of characters to match in s
+ * Return: return count
+ **/
+
 unsigned int _strspn(char *s, char *accept)
+
 {
 	int i, j;
-	unsigned int count = 0;
+	int count = 0;
+	char *str1, *str2;
 
-	for (i = 0; *(accept + i) != '\0'; i++)
-		for (j = 0; *(s + j) != '\0'; j++)
-			if (*(s + i) == *(accept + j))
-				count++;
-	return (count);
+	str1 = s;
+	str2 = accept;
+
+	i = 0;
+	while (str1[i] != '\0') /*Declaring WHILE *s */
+	{
+		j = 0;
+		while (str2[j] != '\0') /*Declaring WHILE *accept*/
+		{
+			if (str2[j] == str1[i]) /*Evaluate condition*/
+			{
+				count++; /*count number*/
+				break;
+			}
+
+			j++;    /*add j+1*/
+		}
+
+		if (s[i] != accept[j]) /*If aren't equals*/
+		{
+			break;
+		}
+
+		i++; /*add x+1*/
+	}
+
+	return (count); /*return the value of count*/
 }
